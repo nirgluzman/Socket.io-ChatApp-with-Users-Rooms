de1924cbd0a4ad5451acb326d40dfecf6340c50e
nirgluzman <nir.gluzman@gmail.com>
2023-11-28 15:09:04 +0100

    add user helper functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Server } from 'socket.io';
 import path from 'path'; // needed for serving static files.
 
 const PORT = process.env.PORT || 3000;
+const ADMIN = 'admin';
 
 // Server initialization with Express, https://socket.io/docs/v4/server-initialization/#with-express
 
@@ -14,46 +15,92 @@ const app = express();
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 // middleware to parse and handle URL-encoded data submitted through HTML forms.
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // start the Express server and listen for incoming connections on PORT.
 const expressServer = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
+
+// Users state
+const UsersState = {
+	users: [],
+	setUsers: function (newUsersArray) {
+		this.users = newUsersArray;
+	},
+};
 
 // create a new Socket.io server instance and binds it to the Express server.
 // CORS is not required, as the Express server is serving the HTML content and the static files.
 const io = new Server(expressServer, {
-  /* options */
+	/* options */
 });
 
 // event handler that is triggered whenever a new client establishes a WebSocket connection with the Socket.io server.
 io.on('connection', (socket) => {
-  console.log(`User ${socket.id} connected!`);
+	console.log(`User ${socket.id} connected!`);
 
-  // Upon connection - send a welcome message ONLY to the new user.
-  socket.emit('message', 'Welcome to Chat App!');
+	// Upon connection - send a welcome message ONLY to the new user.
+	socket.emit('message', 'Welcome to Chat App!');
 
-  // Upon connection - send a message to ALL clients excl. the new user.
-  socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined!`);
+	// Upon connection - send a message to ALL clients excl. the new user.
+	socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined!`);
 
-  // Listening for a message event.
-  socket.on('message', (data) => {
-    console.log(`Received message: ${data}`);
-    // send a message to all clients, include sender.
-    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
-  });
+	// Listening for a message event.
+	socket.on('message', (data) => {
+		console.log(`Received message: ${data}`);
+		// send a message to all clients, include sender.
+		io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+	});
 
-  // Listening for a typing event.
-  socket.on('typing', (name) => {
-    console.log(`${name} is typing ...`);
-    // send a message to all clients, excl. sender.
-    socket.broadcast.emit('typing', name);
-  });
+	// Listening for a typing event.
+	socket.on('typing', (name) => {
+		console.log(`${name} is typing ...`);
+		// send a message to all clients, excl. sender.
+		socket.broadcast.emit('typing', name);
+	});
 
-  // when user disconnects - message to others.
-  socket.on('disconnect', (reason) => {
-    console.log(`User ${socket.id} disconnected! ${reason}`);
-    socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left!`);
-  });
+	// when user disconnects - message to others.
+	socket.on('disconnect', (reason) => {
+		console.log(`User ${socket.id} disconnected! ${reason}`);
+		socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left!`);
+	});
 });
+
+function buildMessage(name, text) {
+	return {
+		name,
+		text,
+		time: new Intl.DateTimeFormat('en-GB', {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			hour12: true,
+			timeZone: 'Europe/Berlin',
+		}).format(new Date()),
+	};
+}
+
+// User functions
+function activateUser(id, name, room) {
+	const user = {id, name, room};
+	UsersState.setUsers([...UsersState.users.filter((user) => user.id != id), user]);
+	return user;
+}
+
+function deactivateUser(id) {
+	UsersState.setUsers(UsersState.users.filter((user) => user.id != id));
+}
+
+function getUser(id) {
+	return UsersState.users.find((user) => user.id == id);
+}
+
+function getUsersInRoom(room) {
+	return UsersState.users.filter((user) => user.room == room);
+}
+
+function getAllActiveRooms() {
+	return Array.from(new Set(UsersState.users.map((user) => user.room))); // Set to avoid duplicants.
+}
+
